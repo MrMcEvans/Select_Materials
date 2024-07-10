6bfacf86664aedfcc504452d5dea8a655949c891
Andrew Evans <anmcev70@gmail.com>
2024-07-10 15:02:18 -0500

    revert

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,38 @@
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
 import App from './App';
 import Error from './pages/Error';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import About from './pages/About';
+import Portfolio from './pages/Portfolio';
 
-const Root = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route index element={<Home />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Contact" element={<Contact />} />
-        <Route path="*" element={<Error />} /> {/* Catch-all route for 404 */}
-      </Route>
-    </Routes>
-  </Router>
-);
 
-ReactDOM.createRoot(document.getElementById('root')).render(<Root />);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/About',
+        element: <About />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+    ],
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />,
+)
